068614f59075de057203f4f543fa2625c24c329a
mayurkukade <mayurkukade@opticalarc.com>
2024-03-12 17:01:34 +0530

    cardetails scrollbar

--- src/pages/CarDetailsById.jsx
+++ src/pages/CarDetailsById.jsx
@@ -5,40 +5,39 @@ import { useGetCarByIdQuery } from "../services/carAPI";
 // import { redirectToSignIn } from "../services/apiSlice";
 import { useNavigate } from "react-router-dom";
 
-
 const CarDetailsById = () => {
-     const navigate = useNavigate();
-    const {carId} = useParams()
-    
-    const {data,isLoading,isError,error} = useGetCarByIdQuery()
-    if(isLoading){
-      return <p>Loading...</p>
-    }
-    console.log(data)
-    console.log(carId)
-    console.log(isLoading)
-    console.log(isError)
-    console.log(error)
-  console.log(error)
-    if(error?.status === 401){
-      console.log('navigate')
-    
-      navigate('/signin')
-    }
+  const navigate = useNavigate();
+  const { carId } = useParams();
 
-    // if (isError && error?.status === 401) {
-    //     console.log('click')
-    //     Cookies.remove('token')
-    //     redirectToSignIn(navigate); // Redirect to sign-in page
-    //     return null; // Stop further rendering
-    // }
+  const { data, isLoading, isError, error } = useGetCarByIdQuery();
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  console.log(data);
+  console.log(carId);
+  console.log(isLoading);
+  console.log(isError);
+  console.log(error);
+  console.log(error);
+  if (error?.status === 401) {
+    console.log("navigate");
+
+    navigate("/signin");
+  }
+
+  // if (isError && error?.status === 401) {
+  //     console.log('click')
+  //     Cookies.remove('token')
+  //     redirectToSignIn(navigate); // Redirect to sign-in page
+  //     return null; // Stop further rendering
+  // }
 
   return (
     <div className="grid grid-flow-row-dense md:grid-cols-3 gap-4 container mx-auto">
-      <div className="p-4 md:col-span-2 overflow-y-auto max-h-screen ">
+      <div className="p-4 md:col-span-2 max-h-screen overflow-scroll no-scrollbar ">
         <CarView />
       </div>
-      <div className="p-4">
+      <div className="p-4 sticky top-0">
         <PriceCard />
       </div>
     </div>
