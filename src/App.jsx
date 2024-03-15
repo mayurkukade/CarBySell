ec89020c353725fa578aec4e537cebac0b6b8095
Vyankatesh1234 <vyankateshkawarase@opticalarc.com>
2024-03-15 18:09:23 +0530

    Vyankatesh Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,31 @@ import { SimpleRegistrationForm } from "./pages/SimpleRegistrationForm";
 
 import BuyCar from "./pages/BuyCar";
 import CarDetailsById from "./pages/CarDetailsById";
+import AboutUs from "./pages/AboutUs";
+import CookiePolicy from "./pages/CookiePolicy";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import ContactUs from "./pages/ContactUs";
 
 export default function App() {
   return (
+    <>
     <Routes>
       <Route path="signin" element={<LoginCard />} />
       <Route path="signup" element={<SimpleRegistrationForm />} />
       <Route element={<AppLayout />}>
         <Route path="/" element={<Home />} />
-
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/cookiepolicy" element={<CookiePolicy />} />
+        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+        <Route path="/contactus" element={<ContactUs />} />
         <Route path="/carlist" element={<BuyCar />} />
         <Route path="/carlist/cardetails/:carId" element={<CarDetailsById />} />
       </Route>
     </Routes>
+    {/* <AboutUs/> */}
+    {/* <CookiePolicy/> */}
+    {/* <PrivacyPolicy/> */}
+    {/* <ContactUs/> */}
+    </>
   );
 }
