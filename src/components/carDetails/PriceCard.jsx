12e417ae768d63ca6b66be0fcbaab99a2dedfb7e
AmitOpticalArc <amitkuril@opticalarc.com>
2024-03-19 11:07:11 +0530

    Amit commit

--- src/components/carDetails/PriceCard.jsx
+++ src/components/carDetails/PriceCard.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import CardUi from "../../ui/CardUi";
-// import { Chip } from "@material-tailwind/react";
+import { Chip } from "@material-tailwind/react";
 
 const PriceCard = ({
   price,
@@ -14,57 +14,83 @@ const PriceCard = ({
   city,
   area,
   color,
-  bodyType
+  bodyType,
 }) => {
-
   return (
     <CardUi>
-      <p className="font-bold text-2xl text-black uppercase">
+      <p className="font-extrabold text-2xl text-black uppercase font-[latto]">
         {year} {brand} {model}
       </p>
-      <p>
+      <p className="uppercase font-[Merriweather]">
         {color} {bodyType} . MANUAL
       </p>
       <div className="my-4 flex gap-2">
-        {/* <Chip value={kmDriven}/> */}
-        <div className="text-sm px-3 py-2 bg-blue-gray-50 rounded-2xl">
-          {kmDriven} KM
-        </div>
-        <div className="text-sm px-3 py-2 bg-blue-gray-50 rounded-2xl">
-          {ownerSerial} Owner
-        </div>
-        <div className="text-sm px-3 py-2 bg-blue-gray-50 rounded-2xl">
-          {fuelType}
-        </div>
-        <div className="text-sm px-3 py-2 bg-blue-gray-50 rounded-2xl">
-          {registration}
+        <Chip
+          variant="outlined"
+          value={`${kmDriven} KM`}
+          className="text-sm text-black font-[latto] hover:bg-gray-900 hover:text-white"
+        />
+        <Chip
+          variant="outlined"
+          value={`${ownerSerial} Owner`}
+          className="text-base text-black font-[latto] hover:bg-gray-900 hover:text-white"
+        />
+        <Chip
+          variant="outlined"
+          value={`${fuelType}`}
+          className="text-base text-black font-[latto] hover:bg-gray-900 hover:text-white"
+        />
+        <Chip
+          variant="outlined"
+          value={`${registration}`}
+          className="text-base text-black font-[latto] hover:bg-gray-900 hover:text-white"
+        />
+      </div>
+      <div className="flex align-bottom items-baseline gap-3">
+        {/* <img alt="home" src={home} className="w-[15px] h-[15px]" /> */}
+        <div className=" mt-4 text-base font-[lotto]">
+          Home Test Drive Available
         </div>
       </div>
-      <div className=" mt-4 text-sm">Home Test Drive Available</div>
-      <div className=" mt-4 text-sm text-blue-900">
-        Parked at : {area},{city}
+      <div className="flex align-bottom items-baseline gap-3">
+        {/* <img alt="home" src={location} className="w-[15px] h-[15px]" /> */}
+        <div className=" mt-4 text-base text-blue-900 font-[lotto]">
+          Parked at: {area},{city}
+        </div>
       </div>
-      <div className=" mt-4 text-sm text-blue-900">View Inspection Report</div>
-      <div className=" mt-4 mb-6 text-sm text-blue-900">
-        Get Service History Report
+      <div className="flex align-bottom items-baseline gap-3">
+        {/* <img alt="home" src={file} className="w-[15px] h-[15px]" /> */}
+        <div className=" mt-4 text-base text-blue-900 font-[lotto]">
+          View Inspection Report
+        </div>
+      </div>
+      <div className="flex align-bottom items-baseline gap-3">
+        {/* <img alt="home" src={whatsapp} className="w-[15px] h-[15px]" /> */}
+        <div className=" mt-4 mb-6 text-base text-blue-900 font-[lotto]">
+          Get Service History Report
+        </div>
       </div>
       <hr className="border-gray-400" />
       <div className="flex justify-between align-middle items-center my-3">
         <div>
-          <div className="text-orange-700 text-xl font-bold">₹6978/month</div>
-          <div className="text-xs">On Zero down Payment</div>
-          <div className="uppercase text-blue-900 text-xs">
+          <div className="text-orange-700 text-xl font-bold font-[latto]">
+            ₹6978/month
+          </div>
+          <div className="text-xs font-[lotto]">On Zero down Payment</div>
+          <div className="uppercase text-blue-900 text-xs font-[lotto]">
             Check Eligibility
           </div>
         </div>
         <div>
-          <div className="text-xl font-bold text-black">₹{price} Lakh</div>
-          <div className="uppercase text-blue-900 text-xs">
+          <div className="text-xl font-bold text-black font-[latto]">
+            ₹{price} Lakh
+          </div>
+          <div className="uppercase text-blue-900 text-xs font-[lotto]">
             Understand Price
           </div>
         </div>
       </div>
-      <div className="my-4 px-6 py-2 bg-orange-700 text-white flex justify-center text-sm items-center rounded-xl uppercase">
+      <div className="my-4 px-6 py-2 bg-orange-700 text-white flex justify-center text-sm items-center rounded-xl uppercase font-[latto]">
         Buy Car
       </div>
     </CardUi>
