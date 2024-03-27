c0d97106777bb096090798eb6a50bb40ec94674e
Vyankatesh1234 <vyankateshkawarase@opticalarc.com>
2024-03-27 11:44:33 +0530

    Venky2 first commit

--- src/pages/UserConfirmBooking.jsx
+++ src/pages/UserConfirmBooking.jsx
@@ -1,5 +1,10 @@
 import { useGetAllUserConfirmQuery } from "../services/carAPI";
 import { Button } from "@material-tailwind/react";
+import {
+  Card,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 const UserConfirmBooking = () => {
   const { data, isLoading, error } = useGetAllUserConfirmQuery();
@@ -8,11 +13,29 @@ const UserConfirmBooking = () => {
   console.log(error);
   return (
     <div>
-      <p>UserConfirmBooking</p>
-      <p>{data?.bookingDto?.price}</p>
-      <Link to={`/carlist/cardetails/${data?.bookingDto?.carId}`}>
-        <Button>Car Details</Button>
-      </Link>
+      <Typography variant="h5" color="blue-gray" className="mb-2 text-center">
+        User Confirm Booking
+      </Typography>
+      <Card className="flex mt-6 w-96 md:ml-[36rem] justify-center items-center">
+        <CardBody>
+
+          <Typography>
+            ID: {data?.bookingDto?.id}
+          </Typography>
+          <Typography>
+            Date: {data?.bookingDto?.date}
+          </Typography>
+          <Typography>
+            Price: {data?.bookingDto?.price}
+          </Typography>
+          <Link to={`/carlist/cardetails/${data?.bookingDto?.carId}`}>
+            <Button>Car Details</Button>
+          </Link>
+        </CardBody>
+        {/* <CardFooter className="pt-0">
+        <Button>Read More</Button>
+      </CardFooter> */}
+      </Card>
     </div>
   );
 };
